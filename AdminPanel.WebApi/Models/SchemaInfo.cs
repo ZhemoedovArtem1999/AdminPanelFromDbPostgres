33468cbc3e6fcdc573cb71fc0d8496635cc5da3e
Zhemoedov Artem Laptop <tema.zhemoedov1999@gmail.com>
2026-03-16 00:28:39 +0300

    Небольшой передизайн

--- AdminPanel.WebApi/Models/SchemaInfo.cs
+++ AdminPanel.WebApi/Models/SchemaInfo.cs
@@ -4,4 +4,7 @@ public class SchemaInfo
 {
     public string SchemaName { get; set; }
     public string Description { get; set; }
+    public string DisplayName => string.IsNullOrWhiteSpace(Description)
+           ? SchemaName
+           : Description;
 }
